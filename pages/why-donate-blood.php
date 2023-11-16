b9ace9f268a9191ea44d4e8cb3e4d0227a9c9fa8
Alex <alexgrubor@gmail.com>
2023-11-16 12:36:22 +0100

    why-donate-blood page

--- pages/why-donate-blood.php
+++ pages/why-donate-blood.php
@@ -1,6 +1,41 @@
-<?php require ('../shared/header.php')?>
+<?php require('../shared/header.php') ?>
+<style>
+    .red-shadow {
+        box-shadow: 0 4px 14px 0 rgba(225, 29, 72, 0.4);
+    }
+</style>
 
-<h1>Why to donate</h1>
+<div class="container mx-auto px-4 py-8">
+    <h1 class="text-4xl font-bold text-red-600 text-center mb-8">Why Donate Blood</h1>
+    <div class="flex justify-center mb-6">
+        <img src="../images/blood_donationcover.jpeg" class="w-3/4 md:w-1/2 lg:w-1/2" alt="why">
+    </div>
+    <section class="mb-6 bg-gray-100 p-4  red-shadow rounded-lg shadow mx-auto w-full md:w-3/4 lg:w-1/2">
+        <h2 class="text-3xl font-semibold mb-2">The Need for Blood Donation</h2>
+        <p class="text-gray-700 text-lg mb-4">Every day, countless lives depend on the availability of blood. Be it for surgeries, chronic illnesses, traumatic injuries, or during childbirth, the demand for blood is constant. By donating blood, you can become a key part of the life-saving process.</p>
+    </section>
+
+    <section class="mb-6 bg-gray-100 p-4  red-shadow rounded-lg shadow mx-auto w-full md:w-3/4 lg:w-1/2">
+        <h2 class="text-3xl font-semibold mb-2">How Your Donation Makes a Difference</h2>
+        <p class="text-gray-700 text-lg mb-4">Each donation can save up to three lives. Your blood can be used to help patients of all ages: children with severe anemia, adults who have lost blood during surgery, or individuals battling cancer. One pint of blood can be the gift of life for someone in need.</p>
+    </section>
+
+    <section class="mb-6 bg-gray-100 p-4  red-shadow rounded-lg shadow mx-auto w-full md:w-3/4 lg:w-1/2">
+        <h2 class="text-3xl font-semibold mb-2">Blood Donation is Safe and Simple</h2>
+        <p class="text-gray-700 text-lg mb-4">Donating blood is a quick, simple, and safe process. It's performed by trained professionals in a controlled, hygienic environment. Every donor undergoes a mini health screening prior to donation, ensuring your safety and the safety of the recipients.</p>
+    </section>
+
+    <section class="mb-6 bg-gray-100 p-4  red-shadow rounded-lg shadow mx-auto w-full md:w-3/4 lg:w-1/2">
+        <h2 class="text-3xl font-semibold mb-2">Join the Community of Donors</h2>
+        <p class="text-gray-700 text-lg mb-4">By donating blood, you join a community of heroes who have chosen to give the gift of life. Your contribution is celebrated and valued, and you become part of a vital network that supports health and saves lives.</p>
+    </section>
+
+    <section class="text-center mx-auto w-full md:w-3/4 lg:w-1/2">
+        <h2 class="text-3xl font-semibold mb-2">Ready to Make a Difference?</h2>
+        <p class="text-gray-700 text-lg mb-6">Your decision to donate blood can have a profound impact. Join us in this life-saving mission.</p>
+        <a href="/pages/become-a-donor.php" class="bg-red-600 text-white font-bold py-2 px-4 rounded hover:bg-red-700 transition-colors">Become a Donor Today</a>
+    </section>
+</div>
 
 
-<?php require ('../shared/footer.php')?>
+<?php require('../shared/footer.php') ?>
